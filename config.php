c096eee6b3dc63d01c2153ad048b89ef5fc46cb7
br3n00320 <62916391+br3n00320@users.noreply.github.com>
2022-06-07 01:52:56 -0300

    atualização

--- config.php
+++ config.php
@@ -1,6 +1,13 @@
 <?php
 
+$dbHost = 'LocalHost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'sistemaprotocolo';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
 
 
 ?>
+
